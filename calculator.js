7dd3d2db3945d23896ae01869ed84a7b9da24328
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2024-03-04 11:12:34 +0900

    Create operation buttons in index.html

--- calculator.js
+++ calculator.js
@@ -1,9 +1,28 @@
+var displayValue = document.querySelector('.display');
 
 
-function calculator(num1, operator, num2) {
-  
+function operate(a, b, ope) {
+  var num1 =a;
+  var num2 =b;
+  var operator = ope;
 
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+    default:
+      return "No operator found";
+  }
 }
+console.log(operate(1, 2, '+'));
+console.log(operate(1, 2, '-'));
+console.log(operate(1, 2, '*'));
+console.log(operate(1, 2, '/'));
 
 function add(num1, num2) {
   return num1+num2;
@@ -12,8 +31,8 @@ function subtract(num1, num2) {
   return num1-num2;
 }
 function multiply(num1, num2) {
-  return num1+num2;
+  return num1*num2;
 }
 function divide(num1, num2) {
-  return num1+num2;
+  return num1/num2;
 }
